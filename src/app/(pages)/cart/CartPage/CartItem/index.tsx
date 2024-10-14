48710a872849c0b1121c205d403c024bf8f62b3a
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-10-14 17:36:26 +0000

    fix: removed unecessary import

--- src/app/(pages)/cart/CartPage/CartItem/index.tsx
+++ src/app/(pages)/cart/CartPage/CartItem/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React, { useState } from 'react'
-import { update } from '@react-spring/web'
 import Link from 'next/link'
 
 import { Media } from '../../../../_components/Media'
